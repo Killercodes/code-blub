214ef04cbc6c6e60e9ec62d0721f8bf5f347e934
KILLER.CODES <Killercodes@users.noreply.github.com>
2018-01-28 17:14:32 +0530

    Update BruteForce.cs

--- C Sharp/BruteForce.cs
+++ C Sharp/BruteForce.cs
@@ -4,10 +4,9 @@ using System.Text;
 
 namespace Killercodes.Cracking 
 { 
-
     public class Bruteforce : IEnumerable 
     { 
-    #region constructors 
+        #region constructors 
         private StringBuilder sb = new StringBuilder();
         //the string we want to permutate 
         public string charset = "abcdefghijklmnopqrstuvwxyz";
@@ -45,8 +44,55 @@ namespace Killercodes.Cracking
             } 
             return sb.ToString(); 
         } 
-    #endregion 
+        #endregion 
     } 
+    
+    public class BruteForce2
+    {
+        // Array with characters to use in Brute Force Algorithm.
+        // You can remove or add more characters in this array.
+        private static char[] fCharList =
+        {
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+            'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+            'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+            'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8',
+            '9', '0','.'
+        };
+
+        /// <summary>
+        /// Start Brute Force.
+        /// </summary>
+        /// <param name="length">Words length.</param>
+        public static void StartBruteForce (int length)
+        {
+            StringBuilder sb = new StringBuilder(length);
+            char currentChar = fCharList[0];
+
+            for (int i = 1; i <= length; i++)
+                sb.Append(currentChar);
+            
+            ChangeCharacters(0, sb, length);
+        }
+
+        private static StringBuilder ChangeCharacters (int pos,StringBuilder sb, int length)
+        {
+            for (int i = 0; i <= fCharList.Length - 1; i++)
+            {
+                sb.Replace(sb[pos], fCharList[i], pos, 1);
+                if (pos == length - 1)
+                {
+                    System.Console.Write("\t");
+                    System.Console.Write(sb.ToString());
+                }
+                else
+                    ChangeCharacters(pos + 1, sb, length);
+            }
+            return sb;
+        }
+    }
 }
 
 
