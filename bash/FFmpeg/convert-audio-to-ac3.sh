95d76ed8474e1b79d1b258491d02e10a9d7f3cfd
KILLER.CODES <Killercodes@users.noreply.github.com>
2020-04-15 21:26:50 +0530

    Update convert-audio-to-ac3.sh

--- bash/FFmpeg/convert-audio-to-ac3.sh
+++ bash/FFmpeg/convert-audio-to-ac3.sh
@@ -5,5 +5,8 @@ echo "
 IN=$1 #input file
 OUT=$2 #output file
 
+HEADER="-hide_banner -y -v quiet -stats "
 
-ffmpeg -i "${IN}" -codec copy -acodec ac3 "${OUT}"
+
+
+ffmpeg $HEADER -i "${IN}" -codec copy -acodec ac3 "${OUT}"
