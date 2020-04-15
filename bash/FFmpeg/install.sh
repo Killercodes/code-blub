759f21f0a301f493ab41304e0bdd050e3c701552
KILLER.CODES <Killercodes@users.noreply.github.com>
2020-04-15 21:29:10 +0530

    Update install.sh

--- bash/FFmpeg/install.sh
+++ bash/FFmpeg/install.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 
+echo "Installing FFmpeg tools"
+
+wget "https://raw.githubusercontent.com/Killercodes/code-blub/master/bash/FFmpeg/convert-audio-to-ac3.sh"
+
+wget "https://raw.githubusercontent.com/Killercodes/code-blub/master/bash/FFmpeg/cnvt-aud-2ch-to-dolby51ch.sh"
